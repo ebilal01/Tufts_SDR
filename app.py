836ae9f28d534ace31db9d17493149f8c43e52fa
ebilal01 <127566393+ebilal01@users.noreply.github.com>
2025-06-30 09:24:04 -0400

    Update app.py

--- app.py
+++ app.py
@@ -92,26 +92,26 @@ def handle_rockblock():
 
         full_data = {
             "received_time": datetime.datetime.utcnow().isoformat() + "Z",
-            "sent_time": sent_time_utc,
+            "sent": sent_time_utc,
             "unix_epoch": message_data.get("unix_epoch", 0),
             "siv": message_data.get("siv", 0),
-            "latitude": float(message_data.get("latitude", 0.0)),
-            "longitude": float(message_data.get("longitude", 0.0)),
-            "altitude": message_data.get("altitude", 0),
-            "pressure_mbar": message_data.get("pressure_mbar", 0),
+            "lat": float(message_data.get("latitude", 0.0)),
+            "lon": float(message_data.get("longitude", 0.0)),
+            "alt": message_data.get("altitude", 0),
+            "pres": message_data.get("pressure_mbar", 0),
             "temperature_pht_c": message_data.get("temperature_pht_c", 0),
-            "temperature_cj_c": message_data.get("temperature_cj_c", 0),
+            "temp_cj_c": message_data.get("temperature_cj_c", 0),
             "temperature_tctip_c": message_data.get("temperature_tctip_c", 0),
             "roll_deg": message_data.get("roll_deg", 0),
             "pitch_deg": message_data.get("pitch_deg", 0),
             "yaw_deg": message_data.get("yaw_deg", 0),
-            "vavg_1_mps": message_data.get("vavg_1_mps", 0),
+            "vavg_1": message_data.get("vavg_1_mps", 0),
             "vavg_2_mps": message_data.get("vavg_2_mps", 0),
             "vavg_3_mps": message_data.get("vavg_3_mps", 0),
-            "vstd_1_mps": message_data.get("vstd_1_mps", 0),
+            "vstd_1": message_data.get("vstd_1_mps", 0),
             "vstd_2_mps": message_data.get("vstd_2_mps", 0),
             "vstd_3_mps": message_data.get("vstd_3_mps", 0),
-            "vpk_1_mps": message_data.get("vpk_1_mps", 0),
+            "vpk_1": message_data.get("vpk_1_mps", 0),
             "vpk_2_mps": message_data.get("vpk_2_mps", 0),
             "vpk_3_mps": message_data.get("vpk_3_mps", 0),
             "message": extra_message
@@ -134,10 +134,10 @@ def get_live_data():
 @app.route('/flight-data', methods=['GET'])
 def live_data():
     data = {
-        "latitude": message_history[-1]["latitude"] if message_history else "No data",
-        "longitude": message_history[-1]["longitude"] if message_history else "No data",
+        "latitude": message_history[-1]["lat"] if message_history else "No data",
+        "longitude": message_history[-1]["lon"] if message_history else "No data",
         "timestamps": message_history[-1]["sent_time"] if message_history else "No data",
-        "altitudes": message_history[-1]["altitude"] if message_history else "No data"
+        "altitudes": message_history[-1]["alt"] if message_history else "No data"
     }
     return jsonify(data)
 
@@ -161,29 +161,6 @@ def download_history():
 def message_history_endpoint():
     return jsonify(message_history) if message_history else jsonify([])
 
-@app.route("/animation-data", methods=['GET'])
-def animation_data():
-    if not message_history:
-        return jsonify({
-            "rotation": 0,
-            "position": {"x": 0, "y": 0, "z": 0},
-            "force": {
-                "x": 0,
-                "y": 0,
-                "z": 0
-            }
-        })
-    latest_message = message_history[-1]
-    telemetry_data = {
-        "rotation": latest_message["yaw_deg"],
-        "position": {"x": 0, "y": 0, "z": 0},
-        "force": {
-            "x": latest_message["vavg_1_mps"],
-            "y": latest_message["vavg_2_mps"],
-            "z": latest_message["vavg_3_mps"]
-        }
-    }
-    return jsonify(telemetry_data)
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
